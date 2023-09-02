06ec6c8170d881a94f19a62e2cba93daa4f72971
NandaPersa <anandapersa@gmail.com>
2023-09-02 10:56:27 -0300

    feat: add toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ToastContainer } from 'react-toastify';
+
 import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  display: "swap",
+  
+})
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   
   return (
   <main className={inter.className}>
     <Component {...pageProps} />
+    <ToastContainer />
   </main>
   )
 };
