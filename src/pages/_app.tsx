82cfc6994c0e761e0896219c4f711798313b9ad7
NandaPersa <anandapersa@gmail.com>
2023-12-18 17:50:12 -0300

    feat: config clarity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,6 +30,18 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     `,
     }}
   />
+   <Script
+    id='teste'
+    type="text/javascript"
+    dangerouslySetInnerHTML={{
+      __html: ` (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "k7xx2pq5fb");
+    `,
+    }}
+  />
         
   <main className={inter.className}>
     <Component {...pageProps} />
